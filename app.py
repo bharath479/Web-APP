2a43d930ee39c0bbf123dbbf9d4ba4f86cf82534
bharath <dadibharath30@gmail.com>
2025-09-02 09:32:23 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_message = request.form['message']
+    return render_template('result.html', message=user_message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
